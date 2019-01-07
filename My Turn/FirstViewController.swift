daed178b2175ae56eda4c41101e11eaa499636c5
Jill <jillss100@yahoo.com>
2019-01-07 14:47:27 -0500

    Initial Commit

---
+++ My Turn/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  My Turn
+//
+//  Created by Jill Uhl on 1/7/19.
+//  Copyright © 2019 Kidlatta. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ My Turn/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  My Turn
+//
+//  Created by Jill Uhl on 1/7/19.
+//  Copyright © 2019 Kidlatta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
